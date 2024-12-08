193d1d2d0a8d876cd66bd132b2751d0a1283748d
DemaPy <benderdema@gmail.com>
2024-12-08 14:53:36 +0100

    Socket initialization

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+
+const io = new Server(server)
 
 // Read public folder in the project in order to get acces for scripts
 // inside index.html file we have js exports
@@ -10,6 +15,14 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log(socket.id, ' CONNECTED')
+
+  socket.on('disconnect', () => {
+    console.log(socket.id, ' DISCONNECTED')
+  })
+})
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
